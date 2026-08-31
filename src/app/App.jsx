a6aab8cd6ca57082d717199a98b9d9017a10fa81
Nikita Karpov <jacquesjaws@yandex.ru>
2026-08-31 21:38:10 +0400

    Иницицализация проекта, создание структуры

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../widgets/Header";
+import Main from "../widgets/Main";
+import Footer from "../widgets/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/widgets/Main/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () =>{
+  return(
+    <main className="main">Content</main>
+  )
+}
+
+export default Main
