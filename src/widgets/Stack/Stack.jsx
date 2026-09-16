90c6365d6748da1b09e4800afdc6f83f1d7a6536
Nikita Karpov <jacquesjaws@yandex.ru>
2026-09-16 17:23:29 +0400

    Стилизовал секцию О себе

---
+++ src/widgets/Stack/Stack.jsx
@@ -0,0 +1,16 @@
+
+import './Stack.scss'
+
+const Stack = (props) => {
+  const {
+    
+  } = props
+  
+  return (
+    <div className="stack">
+      Stack
+    </div>
+  )
+}
+
+export default Stack
